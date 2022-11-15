401247093d2d62772cd4bfacd1a2c73ff743b78d
ingalls <nick@ingalls.ca>
2022-11-15 09:07:41 -0700

    Add Standard Schema

--- api/doc/api.js
+++ api/doc/api.js
@@ -15,3 +15,21 @@
 *
 * @apiSchema {jsonschema=../schema/res.ListSchema.json} apiSuccess
 */
+
+
+/**
+* @api {post} /record Record Stats
+* @apiVersion 1.0.0
+* @apiName POST-/record
+* @apiGroup Record
+* @apiPermission user
+*
+* @apidescription
+*   The daily ETL process will push updates to this endpoint
+*
+
+*
+*
+* @apiSchema (Body) {jsonschema=../schema/req.body.Record.json} apiParam
+* @apiSchema {jsonschema=../schema/res.Standard.json} apiSuccess
+*/
