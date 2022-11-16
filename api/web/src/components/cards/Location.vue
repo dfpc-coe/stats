9a8713733cc7ff250257dfb589721b2d9c6288ac
ingalls <nick@ingalls.ca>
2022-11-16 10:08:45 -0700

    Fix map style

--- api/web/src/components/cards/Location.vue
+++ api/web/src/components/cards/Location.vue
@@ -3,19 +3,9 @@
     <div class="card-body">
         <div class="d-flex">
             <h3 class="card-title">User Locations</h3>
-            <div class="ms-auto">
-                <div class="dropdown">
-                    <a class="dropdown-toggle text-muted" href="#" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Last 7 days</a>
-                    <div class="dropdown-menu dropdown-menu-end">
-                        <a class="dropdown-item active" href="#">Last 7 days</a>
-                        <a class="dropdown-item" href="#">Last 30 days</a>
-                        <a class="dropdown-item" href="#">Last 3 months</a>
-                    </div>
-                </div>
-            </div>
         </div>
         <div class="row">
-            <div id="map"></div>
+            <div id="map" style='height: 350px;'></div>
         </div>
     </div>
 </div>
