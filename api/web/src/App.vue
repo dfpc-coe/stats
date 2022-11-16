2de186789dd69a275d1f5bc5261bd2191dcd5bd1
ingalls <nick@ingalls.ca>
2022-11-16 11:20:57 -0700

    Add API Docs Icon

--- api/web/src/App.vue
+++ api/web/src/App.vue
@@ -6,6 +6,15 @@
                 <div class="page-pretitle">TAK Public Safety</div>
                 <h2 class="page-title">Statistics</h2>
             </div>
+
+            <div class='ms-auto'>
+                <div class='btn-list'>
+                    <a href="/docs/" class="btn btn-dark" target="_blank" rel="noreferrer">
+                        <CodeIcon/>
+                        Docs
+                    </a>
+                </div>
+            </div>
         </div>
     </header>
 
@@ -16,6 +25,9 @@
 <script>
 import '@tabler/core/dist/js/tabler.min.js';
 import '@tabler/core/dist/css/tabler.min.css';
+import {
+    CodeIcon,
+} from 'vue-tabler-icons';
 
 export default {
     name: 'Tak-PS-Stats',
@@ -23,6 +35,9 @@ export default {
         return {
             err: false,
         }
+    },
+    components: {
+        CodeIcon
     }
 }
 </script>
