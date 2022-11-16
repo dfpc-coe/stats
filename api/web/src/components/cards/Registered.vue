dea7989936bd4a9c8aa35149914ee914b2e88515
ingalls <nick@ingalls.ca>
2022-11-16 09:46:56 -0700

    Sketch out select component

--- api/web/src/components/cards/Registered.vue
+++ api/web/src/components/cards/Registered.vue
@@ -4,14 +4,9 @@
         <div class="d-flex">
             <h3 class="card-title">Registered Users</h3>
             <div class="ms-auto">
-                <div class="dropdown">
-                    <a class="dropdown-toggle text-muted" href="#" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Last 7 days</a>
-                    <div class="dropdown-menu dropdown-menu-end">
-                        <a class="dropdown-item active" href="#">Last 7 days</a>
-                        <a class="dropdown-item" href="#">Last 30 days</a>
-                        <a class="dropdown-item" href="#">Last 3 months</a>
-                    </div>
-                </div>
+                <TablerSelect
+                    :values='["Last 30 Days", "Month To Date", "Current Quarter", "Year To Date", "All Time"]'
+                />
             </div>
         </div>
         <div class="row">
@@ -38,6 +33,7 @@
 
 <script>
 import VueApexCharts from 'vue3-apexcharts'
+import TablerSelect from '../util/Select.vue';
 
 export default {
     name: 'RegisteredCard',
@@ -63,6 +59,7 @@ export default {
         }
     },
     components: {
+        TablerSelect,
         ApexChart: VueApexCharts
     }
 }
---
+++ api/web/src/components/util/Select.vue
@@ -0,0 +1,28 @@
+<template>
+<div class="dropdown">
+    <a class="dropdown-toggle text-muted" href="#" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-text='current'></a>
+    <div class="dropdown-menu dropdown-menu-end">
+        <a :key='value' v-for='value in values' class="dropdown-item active" href="#" v-text='value'></a>
+    </div>
+</div>
+</template>
+
+<script>
+export default {
+    name: 'TablerSelect',
+    props: {
+        default: String,
+        values: Array
+    },
+    data: function() {
+        return {
+            current: ''
+        }
+    },
+    mounted: function() {
+        if (!this.default) this.current = this.values[0];
+    },
+    methods: {
+    },
+}
+</script>
