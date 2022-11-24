178eaa3ab218040d841fb9c20356f5a7b1effb08
ingalls <nick@ingalls.ca>
2022-11-24 11:40:45 -0700

    Get ZipCode data displaying in stats map

--- api/web/src/components/cards/Location.vue
+++ api/web/src/components/cards/Location.vue
@@ -8,10 +8,13 @@
             <div id="map" style='height: 350px;'></div>
         </div>
     </div>
+
+     <Err v-if='err' :err='err' @close='err = null'/>
 </div>
 </template>
 
 <script>
+import Err from '../Err.vue';
 import mapboxgl from 'mapbox-gl'
 import 'mapbox-gl/dist/mapbox-gl.css';
 
@@ -19,12 +22,14 @@ export default {
     name: 'LocationCard',
     data: function() {
         return {
+            err: false,
+            tilejson: {},
             map: false,
             scale: 7,
         }
     },
-    mounted: function() {
-        this.fetch();
+    mounted: async function() {
+        await this.fetchJson();
         this.$nextTick(() => { this.mountMap(); });
     },
     methods: {
@@ -32,15 +37,47 @@ export default {
             mapboxgl.accessToken = 'pk.eyJ1IjoiaW5nYWxscyIsImEiOiJsUDF2STRrIn0.S0c3ZNH4HmseIdPXY-CTlA';
             this.map = new mapboxgl.Map({
                 container: 'map',
-                style: 'mapbox://styles/mapbox/streets-v11',
-                center: [-74.5, 40],
+                center: [ -105.477959, 39.116007 ],
                 zoom: 9,
                 projection: 'globe'
             });
+
+            this.map.on('load', () => {
+                this.map.addSource('zipcodes', {
+                    type: 'vector',
+                    tiles: this.tilejson.tiles,
+                    minzoom: this.tilejson.minzoom,
+                    maxzoom: this.tilejson.maxzoom
+                });
+
+                this.map.addLayer({
+                    'id': 'zipcodes',
+                    'type': 'line',
+                    'source': 'zipcodes',
+                    'source-layer': 'tl_2022_us_zcta520',
+                    'layout': {
+                        'line-cap': 'round',
+                        'line-join': 'round'
+                    },
+                    'paint': {
+                        'line-opacity': 0.6,
+                        'line-color': 'rgb(53, 175, 109)',
+                        'line-width': 2
+                    }
+                });
+            });
         },
-        fetch: async function() {
-            //const list = await window.std('/api/location');
+        fetchJson: async function() {
+            try {
+                this.tilejson = await window.std('/api/zipcodes');
+                this.tilejson.tiles.push(String(window.stdurl('/api/zipcodes')) + '/{z}/{x}/{y}');
+            } catch (err) {
+                this.err = err;
+            }
         },
+    },
+    components: {
+        Err
     }
 }
 </script>
