5d6ae38ea713c9ef5d04f11ca77c26922149f038
ingalls <nick@ingalls.ca>
2022-12-30 22:53:14 -0400

    Setup username & password

--- cloudformation/stats.template.js
+++ cloudformation/stats.template.js
@@ -25,6 +25,14 @@ export default cf.merge(
                 Type: 'String',
                 Description: 'Email to send alarms to'
             },
+            Username: {
+                Type: 'String',
+                Description: 'Temporary Username until LDAP lands'
+            },
+            Password: {
+                Type: 'String',
+                Description: 'Temporary Password until LDAP lands'
+            },
             VPC: {
                 Description: 'VPC ID to deploy into',
                 Type: 'String'
